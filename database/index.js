16ff9b124b576472942dcccec485164f09195569
Jennifer Giri <jenniferanngiri@gmail.com>
2022-03-04 18:55:34 -0500

    work on queries

--- database/index.js
+++ database/index.js
@@ -16,19 +16,21 @@ const getProducts = (callback) => {
 }
 
 const getStyles = (productId, callback) => {
-  db.query(`SELECT * FROM styles WHERE "productId"=${productId}`, callback)
+  const photosQuery = `(select json_agg(pics) from (select photos.url, photos.thumbnail_url from photos where "styleId"= s.id) pics) as photos`
+  const skusQuery = `(select json_agg(sku) from (select skus.quantity, skus.size from skus where "style_id"= s.id) sku) as skus from styles as s`
+  db.query(`select row_to_json(stys) as styles from (select s.id, s.product_id, s.name, s.sale_price, s.original_price, s.default_style, ${photosQuery}, ${skusQuery}) stys where product_id=${productId}`, callback)
 }
 
 const getFeatures = (productId, callback) => {
-  db.query(`SELECT * FROM features where "productId"=${productId}`)
+  db.query(`select row_to_json(prods) as products from (select p.id, p.name, p.slogan, p.description, p.category, p.default_price, (select json_agg(feat) from (select * from features where "product_id"= p.id) feat) as features from products as p) prods where id=${productId}`, callback)
 }
 
 const getSkus = (styleId, callback) => {
-  db.query(`SELECT * FROM skus where "style_id"=${styleId}`)
+  db.query(`SELECT * FROM skus where "style_id"=${styleId}`, callback)
 }
 //NOTE in db one is referred as styleId and the other is style_id
 const getPhotos = (styleId, callback) => {
-  db.query(`SELECT * FROM photos where "styleId"=${styleId}`)
+  db.query(`SELECT * FROM photos where "styleId"=${styleId}`, callback)
 }
 
 module.exports = {
